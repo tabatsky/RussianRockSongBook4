a71c639f93d98f9168890b205a07051e0daefdb2
tabatsky <e.tabatsky@gmail.com>
2021-11-16 17:33:21 +1000

    MusicHelper created.

---
+++ app/src/main/java/jatx/russianrocksongbook/music/MusicHelper.kt
@@ -0,0 +1,66 @@
+package jatx.russianrocksongbook.music
+
+import android.app.Activity
+import android.content.ActivityNotFoundException
+import android.content.Intent
+import android.net.Uri
+import jatx.russianrocksongbook.R
+import jatx.russianrocksongbook.viewmodel.MvvmViewModel
+import java.io.UnsupportedEncodingException
+import java.net.URLEncoder
+import javax.inject.Inject
+
+class MusicHelper @Inject constructor(
+    val mainActivity: Activity,
+    val mvvmViewModel: MvvmViewModel
+) {
+    fun openYandexMusic(searchFor: String) {
+        try {
+            with(mainActivity) {
+                val searchForEncoded = URLEncoder.encode(searchFor.replace(" ", "+"), "UTF-8")
+                val uri = "https://music.yandex.ru/search?text=$searchForEncoded"
+                startActivity(
+                    Intent.createChooser(
+                        Intent(Intent.ACTION_VIEW, Uri.parse(uri)),
+                        getString(R.string.search_at_yandex_music)
+                    )
+                )
+            }
+        } catch (e: UnsupportedEncodingException) {
+            mvvmViewModel.showToast(R.string.utf8_not_supported)
+        }
+    }
+
+    fun openVkMusic(searchFor: String) {
+        try {
+            with(mainActivity) {
+                val searchForEncoded = URLEncoder.encode(searchFor, "UTF-8")
+                val uri = "https://vk.com/audio?q=$searchForEncoded"
+                val intent = Intent(Intent.ACTION_VIEW, Uri.parse(uri))
+                intent.setPackage("com.vkontakte.android")
+                startActivity(intent)
+            }
+        } catch (e: UnsupportedEncodingException) {
+            mvvmViewModel.showToast(R.string.utf8_not_supported)
+        } catch (e: ActivityNotFoundException) {
+            mvvmViewModel.showToast(R.string.vk_app_not_installed)
+        }
+    }
+
+    fun openYoutubeMusic(searchFor: String) {
+        try {
+            with(mainActivity) {
+                val searchForEncoded = URLEncoder.encode(searchFor.replace(" ", "+"), "UTF-8")
+                val uri = "https://music.youtube.com/search?q=$searchForEncoded"
+                startActivity(
+                    Intent.createChooser(
+                        Intent(Intent.ACTION_VIEW, Uri.parse(uri)),
+                        getString(R.string.search_at_youtube_music)
+                    )
+                )
+            }
+        } catch (e: UnsupportedEncodingException) {
+            mvvmViewModel.showToast(R.string.utf8_not_supported)
+        }
+    }
+}
