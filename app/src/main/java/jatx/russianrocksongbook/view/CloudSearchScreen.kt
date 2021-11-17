20ce81bfffa5a8512d5fe4daad79ecee8a6e1f71
tabatsky <tabatsky@users.noreply.github.com>
2021-11-18 08:21:24 +1000

    CloudSearchScreen refactored. some import changes in SongListScreen.

--- app/src/main/java/jatx/russianrocksongbook/view/CloudSearchScreen.kt
+++ app/src/main/java/jatx/russianrocksongbook/view/CloudSearchScreen.kt
@@ -16,11 +16,13 @@ import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontStyle
-import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.TextUnit
 import androidx.compose.ui.unit.dp
 import jatx.russianrocksongbook.R
 import jatx.russianrocksongbook.data.OrderBy
+import jatx.russianrocksongbook.domain.CloudSong
 import jatx.russianrocksongbook.preferences.ScalePow
+import jatx.russianrocksongbook.preferences.Theme
 import jatx.russianrocksongbook.viewmodel.MvvmViewModel
 import kotlinx.coroutines.launch
 
@@ -63,83 +65,22 @@ fun CloudSearchScreen(mvvmViewModel: MvvmViewModel) {
             },
             backgroundColor = theme.colorCommon,
             navigationIcon = {
-                IconButton(onClick = {
-                    mvvmViewModel.back { }
-                }) {
-                    Icon(painterResource(id = R.drawable.ic_back), "")
-                }
+                CommonNavigationIcon(mvvmViewModel)
             }
         )
 
-        val size1 = dimensionResource(id = R.dimen.search_button_size) * 0.5f
-        val size2 = dimensionResource(id = R.dimen.search_button_size) * 0.75f
-        val size3 = dimensionResource(id = R.dimen.search_button_size) * 1.25f
-        val padding = dimensionResource(id = R.dimen.empty)
-        Row(
-            modifier = Modifier
-                .fillMaxWidth()
-                .height(size3)
-        ) {
-            Column(
-                modifier = Modifier
-                    .weight(1.0f)
-                    .fillMaxHeight()
-            ) {
-                TextField(
-                    value = searchFor,
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .height(size2)
-                        .padding(padding),
-                    colors = TextFieldDefaults
-                        .textFieldColors(
-                            backgroundColor = theme.colorMain,
-                            textColor = theme.colorBg
-                        ),
-                    textStyle = TextStyle(
-                        fontSize = fontSizeTextSp
-                    ),
-                    onValueChange = {
-                        searchFor = it
-                    }
-                )
-                Divider(
-                    color = theme.colorBg,
-                    thickness = padding
-                )
-                Spinner(
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .height(size1)
-                        .padding(padding),
-                    theme = theme,
-                    fontSize = fontSizeTextSp,
-                    valueList = OrderBy.values().map { it.orderByRus }.toTypedArray(),
-                    initialPosition = 0
-                ) {
-                    orderBy = OrderBy.values()[it]
-                }
-            }
-            Box (
-                modifier = Modifier
-                    .width(size3)
-                    .height(size3)
-                    .padding(padding)
-            ) {
-                IconButton(
-                    modifier = Modifier
-                        .background(theme.colorCommon)
-                        .fillMaxSize(),
-                    onClick = onSearchClick
-                ) {
-                    Icon(
-                        painter = painterResource(id = R.drawable.ic_cloud_search),
-                        contentDescription = "",
-                        modifier = Modifier.padding(10.dp)
-                    )
-                }
-            }
-        }
+        CloudSearchPanel(
+            searchFor = searchFor,
+            theme = theme,
+            fontSizeTextSp = fontSizeTextSp,
+            onSearchForValueChange = {
+                searchFor = it
+            },
+            onOrderByValueChange = {
+                orderBy = it
+            },
+            onSearchClick = onSearchClick
+        )
 
         if (!isCloudLoading) {
             if (cloudSongList.isNotEmpty()) {
@@ -151,42 +92,12 @@ fun CloudSearchScreen(mvvmViewModel: MvvmViewModel) {
                     state = listState
                 ) {
                     itemsIndexed(cloudSongList) { index, cloudSong ->
-                        Column(
-                            modifier = Modifier
-                                .fillMaxWidth()
-                                .wrapContentHeight()
-                                .clickable {
-                                    println("selected: ${cloudSong.artist} - ${cloudSong.title}")
-                                    mvvmViewModel.selectCloudSong(index)
-                                    mvvmViewModel.selectScreen(CurrentScreenVariant.CLOUD_SONG_TEXT)
-                                }
+                        CloudSongItem(
+                            cloudSong, theme, fontSizeArtistSp, fontSizeSongTitleSp
                         ) {
-                            Text(
-                                text = cloudSong.visibleTitleWithRating,
-                                modifier = Modifier
-                                    .fillMaxWidth()
-                                    .padding(
-                                        top = dimensionResource(id = R.dimen.padding_8),
-                                        bottom = dimensionResource(id = R.dimen.padding_8),
-                                        start = dimensionResource(id = R.dimen.padding_20)
-                                    ),
-                                fontSize = fontSizeSongTitleSp,
-                                color = theme.colorMain
-                            )
-                            Text(
-                                text = cloudSong.artist,
-                                modifier = Modifier
-                                    .fillMaxWidth()
-                                    .padding(
-                                        top = dimensionResource(id = R.dimen.padding_8),
-                                        bottom = dimensionResource(id = R.dimen.padding_8),
-                                        start = dimensionResource(id = R.dimen.padding_20)
-                                    ),
-                                fontSize = fontSizeArtistSp,
-                                fontStyle = FontStyle.Italic,
-                                color = theme.colorMain
-                            )
-                            Divider(color = theme.colorCommon, thickness = 1.dp)
+                            println("selected: ${cloudSong.artist} - ${cloudSong.title}")
+                            mvvmViewModel.selectCloudSong(index)
+                            mvvmViewModel.selectScreen(CurrentScreenVariant.CLOUD_SONG_TEXT)
                         }
                     }
                     coroutineScope.launch {
@@ -194,33 +105,152 @@ fun CloudSearchScreen(mvvmViewModel: MvvmViewModel) {
                     }
                 }
             } else {
-                Column(
-                    modifier = Modifier.fillMaxSize(),
-                    verticalArrangement = Arrangement.Center,
-                    horizontalAlignment = Alignment.CenterHorizontally
-                ) {
-                    Text(
-                        text = stringResource(id = R.string.label_placeholder),
-                        textAlign = TextAlign.Center,
-                        fontSize = fontSizeSongTitleSp,
-                        color = theme.colorMain
-                    )
-                }
+                CommonSongListStub(fontSizeSongTitleSp, theme)
             }
         } else {
-            Column(
-                modifier = Modifier.fillMaxSize(),
-                verticalArrangement = Arrangement.Center,
-                horizontalAlignment = Alignment.CenterHorizontally
+            CloudSearchProgress(theme)
+        }
+    }
+}
+
+@Composable
+private fun CloudSearchPanel(
+    searchFor: String,
+    theme: Theme,
+    fontSizeTextSp: TextUnit,
+    onSearchForValueChange: (String) -> Unit,
+    onOrderByValueChange: (OrderBy) -> Unit,
+    onSearchClick: () -> Unit
+) {
+    val size1 = dimensionResource(id = R.dimen.search_button_size) * 0.5f
+    val size2 = dimensionResource(id = R.dimen.search_button_size) * 0.75f
+    val size3 = dimensionResource(id = R.dimen.search_button_size) * 1.25f
+    val padding = dimensionResource(id = R.dimen.empty)
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(size3)
+    ) {
+        Column(
+            modifier = Modifier
+                .weight(1.0f)
+                .fillMaxHeight()
+        ) {
+            TextField(
+                value = searchFor,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(size2)
+                    .padding(padding),
+                colors = TextFieldDefaults
+                    .textFieldColors(
+                        backgroundColor = theme.colorMain,
+                        textColor = theme.colorBg
+                    ),
+                textStyle = TextStyle(
+                    fontSize = fontSizeTextSp
+                ),
+                onValueChange = onSearchForValueChange
+            )
+            Divider(
+                color = theme.colorBg,
+                thickness = padding
+            )
+            Spinner(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(size1)
+                    .padding(padding),
+                theme = theme,
+                fontSize = fontSizeTextSp,
+                valueList = OrderBy.values().map { it.orderByRus }.toTypedArray(),
+                initialPosition = 0
             ) {
-                CircularProgressIndicator(
-                    modifier = Modifier
-                        .width(100.dp)
-                        .height(100.dp)
-                        .background(theme.colorBg),
-                    color = theme.colorMain
+                onOrderByValueChange(OrderBy.values()[it])
+            }
+        }
+        Box (
+            modifier = Modifier
+                .width(size3)
+                .height(size3)
+                .padding(padding)
+        ) {
+            IconButton(
+                modifier = Modifier
+                    .background(theme.colorCommon)
+                    .fillMaxSize(),
+                onClick = onSearchClick
+            ) {
+                Icon(
+                    painter = painterResource(id = R.drawable.ic_cloud_search),
+                    contentDescription = "",
+                    modifier = Modifier.padding(10.dp)
                 )
             }
         }
+    }
+}
+
+@Composable
+private fun CloudSongItem(
+    cloudSong: CloudSong,
+    theme: Theme,
+    fontSizeArtistSp: TextUnit,
+    fontSizeSongTitleSp: TextUnit,
+    onClick: () -> Unit
+) {
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .wrapContentHeight()
+            .clickable {
+                onClick()
+            }
+    ) {
+        Text(
+            text = cloudSong.visibleTitleWithRating,
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(
+                    top = dimensionResource(id = R.dimen.padding_8),
+                    bottom = dimensionResource(id = R.dimen.padding_8),
+                    start = dimensionResource(id = R.dimen.padding_20)
+                ),
+            fontSize = fontSizeSongTitleSp,
+            color = theme.colorMain
+        )
+        Text(
+            text = cloudSong.artist,
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(
+                    top = dimensionResource(id = R.dimen.padding_8),
+                    bottom = dimensionResource(id = R.dimen.padding_8),
+                    start = dimensionResource(id = R.dimen.padding_20)
+                ),
+            fontSize = fontSizeArtistSp,
+            fontStyle = FontStyle.Italic,
+            color = theme.colorMain
+        )
+        Divider(color = theme.colorCommon, thickness = 1.dp)
+    }
+}
+
+@Composable
+private fun CloudSearchProgress(
+    theme: Theme
+) {
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        CircularProgressIndicator(
+            modifier = Modifier
+                .width(100.dp)
+                .height(100.dp)
+                .background(theme.colorBg),
+            color = theme.colorMain
+        )
     }
 }
--- app/src/main/java/jatx/russianrocksongbook/view/SongListScreen.kt
+++ app/src/main/java/jatx/russianrocksongbook/view/SongListScreen.kt
@@ -9,14 +9,12 @@ import androidx.compose.foundation.lazy.itemsIndexed
 import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.material.*
 import androidx.compose.runtime.*
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.TextUnit
 import androidx.compose.ui.unit.dp
 import jatx.russianrocksongbook.R
