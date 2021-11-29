7345fb13068d0332b91c1cfe9776a78bef340a20
tabatsky <tabatsky@users.noreply.github.com>
2021-11-29 19:51:42 +1000

    @Singleton changed to @ActivityRetainedScoper in StateHolder's.

--- addSong/src/main/java/jatx/russianrocksongbook/addsong/viewmodel/AddSongStateHolder.kt
+++ addSong/src/main/java/jatx/russianrocksongbook/addsong/viewmodel/AddSongStateHolder.kt
@@ -1,12 +1,12 @@
 package jatx.russianrocksongbook.addsong.viewmodel
 
+import dagger.hilt.android.scopes.ActivityRetainedScoped
 import jatx.russianrocksongbook.model.domain.Song
 import jatx.russianrocksongbook.viewmodel.CommonStateHolder
 import kotlinx.coroutines.flow.MutableStateFlow
 import javax.inject.Inject
-import javax.inject.Singleton
 
-@Singleton
+@ActivityRetainedScoped
 class AddSongStateHolder @Inject constructor(
     val commonStateHolder: CommonStateHolder
 ) {
