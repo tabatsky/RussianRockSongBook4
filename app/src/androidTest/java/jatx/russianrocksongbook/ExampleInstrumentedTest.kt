4b8038c90da237cd2d6eb23abdc7a7a1b9258126
tabatsky <tabatsky@users.noreply.github.com>
2021-12-03 16:56:20 +1000

    android testing added.

--- app/src/androidTest/java/jatx/russianrocksongbook/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/jatx/russianrocksongbook/ExampleInstrumentedTest.kt
@@ -1,22 +1,81 @@
 package jatx.russianrocksongbook
 
-import androidx.test.ext.junit.runners.AndroidJUnit4
-import androidx.test.platform.app.InstrumentationRegistry
-import org.junit.Assert.assertEquals
+import android.app.Application
+import android.content.Context
+import android.util.Log
+import androidx.compose.ui.test.*
+import androidx.compose.ui.test.junit4.createAndroidComposeRule
+import androidx.test.runner.AndroidJUnitRunner
+import dagger.hilt.android.testing.HiltAndroidRule
+import dagger.hilt.android.testing.HiltAndroidTest
+import dagger.hilt.android.testing.HiltTestApplication
+import jatx.russianrocksongbook.view.CurrentScreen
+import org.junit.Before
+import org.junit.Rule
 import org.junit.Test
-import org.junit.runner.RunWith
+
 
 /**
  * Instrumented test, which will execute on an Android device.
  *
  * See [testing documentation](http://d.android.com/tools/testing).
  */
-@RunWith(AndroidJUnit4::class)
+
+class HiltTestRunner : AndroidJUnitRunner() {
+    override fun newApplication(
+        cl: ClassLoader?,
+        className: String?,
+        context: Context?,
+    ): Application {
+        return super.newApplication(cl, HiltTestApplication::class.java.name, context)
+    }
+}
+
+@ExperimentalTestApi
+@HiltAndroidTest
 class ExampleInstrumentedTest {
+    @get:Rule
+    var hiltRule = HiltAndroidRule(this)
+
+    @get:Rule
+    val composeTestRule = createAndroidComposeRule<MainActivity>()
+
+    @Before
+    fun init() {
+        hiltRule.inject()
+        composeTestRule.setContent {
+            CurrentScreen()
+        }
+    }
+
     @Test
-    fun useAppContext() {
-        // Context of the app under test.
-        val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("jatx.russianrocksongbook", appContext.packageName)
+    fun menuAndSongListTest() {
+        composeTestRule.onNodeWithTag("drawerButtonMain").performClick()
+        Log.e("test click", "drawerButtonMain")
+        composeTestRule.onNodeWithText("Избранное").assertIsDisplayed()
+        Log.e("test assert", "Избранное is displayed")
+        composeTestRule.onNodeWithText("Добавить исполнителя").assertIsDisplayed()
+        Log.e("test assert", "Добавить исполнителя is displayed")
+        composeTestRule.onNodeWithText("Добавить песню").assertIsDisplayed()
+        Log.e("test assert", "Добавить песню is displayed")
+        composeTestRule.onNodeWithText("Аккорды онлайн").assertIsDisplayed()
+        Log.e("test assert", "Аккорды онлайн is displayed")
+        composeTestRule.onNodeWithText("Пожертвования").assertIsDisplayed()
+        Log.e("test assert", "Пожертвования is displayed")
+        composeTestRule.onNodeWithText("Немного Нервно").assertDoesNotExist()
+        Log.e("test assert", "Немного Нервно does not exist")
+        composeTestRule.onNodeWithTag("menuLazyColumn").performScrollToIndex(30)
+        Log.e("test scroll", "menu to index 30")
+        composeTestRule.onNodeWithText("Евгений Александрович").assertIsDisplayed()
+        Log.e("test assert", "Евгений Александрович is displayed")
+        composeTestRule.onNodeWithTag("menuLazyColumn").performScrollToIndex(40)
+        Log.e("test scroll", "menu to index 40")
+        composeTestRule.onNodeWithText("Немного Нервно").assertIsDisplayed()
+        Log.e("test assert", "Немного Нервно is displayed")
+        composeTestRule.onNodeWithText("Немного Нервно").performClick()
+        composeTestRule.onNodeWithText("Santa Maria").assertIsDisplayed()
+        Log.e("test assert", "Santa Maria is displayed")
+        composeTestRule.onNodeWithText("Яблочный остров").assertDoesNotExist()
+        Log.e("test assert", "Яблочный остров does not exist")
     }
 }
