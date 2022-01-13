9845ca33f725c70d338ab5c3bb41fbb2267fafc1
Evgeny Tabatsky <e.tabatsky@gmail.com>
2022-01-13 15:07:42 +1000

    LocalRepositoryInitializer via Flow

--- domain/src/main/java/jatx/russianrocksongbook/domain/repository/init/LocalRepositoryInitializer.kt
+++ domain/src/main/java/jatx/russianrocksongbook/domain/repository/init/LocalRepositoryInitializer.kt
@@ -1,7 +1,9 @@
 package jatx.russianrocksongbook.domain.repository.init
 
+import kotlinx.coroutines.flow.Flow
+
 interface LocalRepositoryInitializer {
-    fun fillDbFromJSON(onProgressChanged: (Int, Int) -> Unit)
+    fun fillDbFromJSON(): Flow<Pair<Int, Int>>
     fun applySongPatches()
     fun deleteWrongSongs()
     fun deleteWrongArtists()
