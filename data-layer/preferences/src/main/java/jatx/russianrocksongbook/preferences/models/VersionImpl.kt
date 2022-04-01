a5b955f2203f5150815c4e63458227c0cc14cb13
Evgeny Tabatsky <e.tabatsky@gmail.com>
2022-04-02 07:47:35 +1000

    suppress deprecation.

--- data-layer/preferences/src/main/java/jatx/russianrocksongbook/preferences/models/VersionImpl.kt
+++ data-layer/preferences/src/main/java/jatx/russianrocksongbook/preferences/models/VersionImpl.kt
@@ -5,10 +5,11 @@ import android.content.pm.PackageManager
 import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
 import it.czerwinski.android.hilt.annotations.BoundTo
-import jatx.russianrocksongbook.domain.models.version.Version
+import jatx.russianrocksongbook.domain.models.appcrash.Version
 import javax.inject.Inject
 import javax.inject.Singleton
 
+@Suppress("DEPRECATION")
 @Singleton
 @BoundTo(supertype = Version::class, component = SingletonComponent::class)
 class VersionImpl @Inject constructor(
