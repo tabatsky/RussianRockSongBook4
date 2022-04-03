f97bfa843a146c7c7e7d3afcff9ea85ef01adbab
Evgeny Tabatsky <e.tabatsky@gmail.com>
2022-04-04 09:27:16 +1000

    addSong module files separated.

--- ui-layer/addSong/src/main/java/jatx/russianrocksongbook/addsong/api/view/AddSongScreen.kt
+++ ui-layer/addSong/src/main/java/jatx/russianrocksongbook/addsong/api/view/AddSongScreen.kt
@@ -1,28 +1,22 @@
 package jatx.russianrocksongbook.addsong.api.view
 
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.*
-import androidx.compose.material.*
-import androidx.compose.runtime.*
-import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.foundation.layout.BoxWithConstraints
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.LocalDensity
-import androidx.compose.ui.platform.testTag
-import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.text.font.FontFamily
-import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import jatx.russianrocksongbook.addsong.R
+import jatx.russianrocksongbook.addsong.internal.view.AddSongBody
 import jatx.russianrocksongbook.addsong.internal.viewmodel.AddSongViewModel
 import jatx.russianrocksongbook.commonview.appbar.CommonSideAppBar
 import jatx.russianrocksongbook.commonview.appbar.CommonTopAppBar
 import jatx.russianrocksongbook.commonview.dialogs.confirm.UploadDialog
-import jatx.russianrocksongbook.domain.repository.preferences.ScalePow
-import jatx.russianrocksongbook.testing.TEXT_FIELD_ARTIST
-import jatx.russianrocksongbook.testing.TEXT_FIELD_TEXT
-import jatx.russianrocksongbook.testing.TEXT_FIELD_TITLE
 
 @Composable
 fun AddSongScreen() {
@@ -75,140 +69,3 @@ fun AddSongScreen() {
     }
 }
 
-@Composable
-private fun AddSongBody() {
-    val addSongViewModel: AddSongViewModel = viewModel()
-
-    var artist by rememberSaveable { mutableStateOf("") }
-    var title by rememberSaveable { mutableStateOf("") }
-    var text by rememberSaveable { mutableStateOf("") }
-
-    val theme = addSongViewModel.settings.theme
-
-    val fontScale = addSongViewModel.settings.getSpecificFontScale(ScalePow.TEXT)
-    val fontSizeTextDp = dimensionResource(id = R.dimen.text_size_16) * fontScale
-    val fontSizeTextSp = with(LocalDensity.current) {
-        fontSizeTextDp.toSp()
-    }
-
-    Column(
-        modifier = Modifier
-            .fillMaxSize()
-            .padding(4.dp)
-    ) {
-        TextField(
-            value = artist,
-            onValueChange = {
-                artist = it
-            },
-            modifier = Modifier
-                .testTag(TEXT_FIELD_ARTIST)
-                .fillMaxWidth()
-                .wrapContentHeight(),
-            label = {
-                Text(
-                    text = stringResource(id = R.string.field_artist),
-                    color = theme.colorBg,
-                    fontSize = fontSizeTextSp * 0.7f
-                )
-            },
-            colors = TextFieldDefaults
-                .textFieldColors(
-                    backgroundColor = theme.colorMain,
-                    textColor = theme.colorBg
-                ),
-            textStyle = TextStyle(
-                fontFamily = FontFamily.Monospace,
-                fontSize = fontSizeTextSp
-            )
-        )
-        Divider(
-            color = theme.colorBg,
-            thickness = 4.dp
-        )
-        TextField(
-            value = title,
-            onValueChange = {
-                title = it
-            },
-            modifier = Modifier
-                .testTag(TEXT_FIELD_TITLE)
-                .fillMaxWidth()
-                .wrapContentHeight(),
-            label = {
-                Text(
-                    text = stringResource(id = R.string.field_title),
-                    color = theme.colorBg,
-                    fontSize = fontSizeTextSp * 0.7f
-                )
-            },
-            colors = TextFieldDefaults
-                .textFieldColors(
-                    backgroundColor = theme.colorMain,
-                    textColor = theme.colorBg
-                ),
-            textStyle = TextStyle(
-                fontFamily = FontFamily.Monospace,
-                fontSize = fontSizeTextSp
-            )
-        )
-        Divider(
-            color = theme.colorBg,
-            thickness = 4.dp
-        )
-        TextField(
-            value = text,
-            onValueChange = {
-                text = it
-            },
-            modifier = Modifier
-                .testTag(TEXT_FIELD_TEXT)
-                .fillMaxWidth()
-                .weight(1.0f),
-            label = {
-                Text(
-                    text = stringResource(id = R.string.field_song_text),
-                    color = theme.colorBg,
-                    fontSize = fontSizeTextSp * 0.7f
-                )
-            },
-            colors = TextFieldDefaults
-                .textFieldColors(
-                    backgroundColor = theme.colorMain,
-                    textColor = theme.colorBg
-                ),
-            textStyle = TextStyle(
-                fontFamily = FontFamily.Monospace,
-                fontSize = fontSizeTextSp
-            )
-        )
-        Divider(
-            color = theme.colorBg,
-            thickness = 4.dp
-        )
-        Button(
-            modifier = Modifier
-                .fillMaxWidth()
-                .height(50.dp),
-            colors = ButtonDefaults
-                .buttonColors(
-                    backgroundColor = theme.colorCommon,
-                    contentColor = theme.colorMain
-                ),
-            onClick = {
-                if (
-                    artist.trim().isEmpty() ||
-                    title.trim().isEmpty() ||
-                    text.trim().isEmpty()
-                ) {
-                    addSongViewModel.showToast(R.string.toast_fill_all_fields)
-                } else {
-                    addSongViewModel.addSongToRepo(
-                        artist, title, text
-                    )
-                }
-            }) {
-            Text(text = stringResource(id = R.string.save))
-        }
-    }
-}
