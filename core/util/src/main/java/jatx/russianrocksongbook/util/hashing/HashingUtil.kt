535a81b9cea5ff3d381cc2be7b61532b1080eddc
Evgeny Tabatsky <e.tabatsky@gmail.com>
2022-04-05 10:13:01 +1000

    HashingUtil: some refactoring.

--- core/util/src/main/java/jatx/russianrocksongbook/util/hashing/HashingUtil.kt
+++ core/util/src/main/java/jatx/russianrocksongbook/util/hashing/HashingUtil.kt
@@ -7,25 +7,19 @@ import java.security.MessageDigest
 import java.security.NoSuchAlgorithmException
 
 object HashingUtil {
-    private var digest: MessageDigest? = null
-
-    init {
-        var tmpDigest: MessageDigest?
-        try {
-            tmpDigest = MessageDigest.getInstance("MD5")
-        } catch (e: NoSuchAlgorithmException) {
-            tmpDigest = null
-            e.printStackTrace()
-        }
-        digest = tmpDigest
+    private val digest = try {
+        MessageDigest.getInstance("MD5")
+    } catch (e: NoSuchAlgorithmException) {
+        Log.e("hash", "no MD5 digest found")
+        null
     }
 
-    fun md5(text: String): String = digest?.let {
+    fun hash(text: String): String = digest?.let {
         it.reset()
         it.update(text.toByteArray())
         BigInteger(1, it.digest()).toString(16)
     } ?: run {
-        Log.e("songTextHash", "using random")
+        Log.e("hash", "using random")
         RandomStringUtils.randomAlphanumeric(20)
     }
 }
--- domain-layer/models/local/src/main/java/jatx/russianrocksongbook/domain/models/local/SongTextHash.kt
+++ domain-layer/models/local/src/main/java/jatx/russianrocksongbook/domain/models/local/SongTextHash.kt
@@ -5,5 +5,5 @@ import jatx.russianrocksongbook.util.hashing.HashingUtil
 fun songTextHash(text: String): String {
     val preparedText =
         text.trim { it <= ' ' }.lowercase().replace("\\s+".toRegex(), " ")
-    return HashingUtil.md5(preparedText)
+    return HashingUtil.hash(preparedText)
 }
