59ab3de7312053a9b02a5a4980bfad2137be9136
Evgeny Tabatsky <e.tabatsky@gmail.com>
2022-04-09 08:22:26 +1000

    Warning primary constructor became private.

--- domain-layer/models/warning/src/main/java/jatx/russianrocksongbook/domain/models/warning/Warning.kt
+++ domain-layer/models/warning/src/main/java/jatx/russianrocksongbook/domain/models/warning/Warning.kt
@@ -6,27 +6,28 @@ import jatx.russianrocksongbook.domain.models.local.Song
 const val TYPE_CLOUD = "cloud"
 const val TYPE_OUT_OF_THE_BOX = "outOfTheBox"
 
-data class Warning(
-    var warningType: String = TYPE_OUT_OF_THE_BOX,
-    var artist: String = "",
-    var title: String = "",
-    var variant: Int = 0,
-    var comment: String = ""
+@Suppress("DataClassPrivateConstructor")
+data class Warning private constructor(
+    var warningType: String,
+    var artist: String,
+    var title: String,
+    var variant: Int,
+    var comment: String
 ) {
 
-    constructor(song: Song, comment: String): this() {
-        warningType = TYPE_OUT_OF_THE_BOX
-        artist = song.artist
-        title = song.title
-        variant = -1
-        this.comment = comment
-    }
+    constructor(song: Song, comment: String): this(
+        warningType = TYPE_OUT_OF_THE_BOX,
+        artist = song.artist,
+        title = song.title,
+        variant = -1,
+        comment = comment
+    )
 
-    constructor(cloudSong: CloudSong, comment: String): this() {
-        warningType = TYPE_CLOUD
-        artist = cloudSong.artist
-        title = cloudSong.title
-        variant = cloudSong.variant
-        this.comment = comment
-    }
+    constructor(cloudSong: CloudSong, comment: String): this(
+        warningType = TYPE_CLOUD,
+        artist = cloudSong.artist,
+        title = cloudSong.title,
+        variant = cloudSong.variant,
+        comment = comment
+    )
 }
