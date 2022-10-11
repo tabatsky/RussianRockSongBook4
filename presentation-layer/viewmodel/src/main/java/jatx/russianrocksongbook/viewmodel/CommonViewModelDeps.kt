73060c95d4ab655681365e4429c3adfd22559026
Evgeniy Tabatsky <tabatsky.evgeniy@gene.com>
2022-10-12 09:02:56 +1000

    TVDetector

--- presentation-layer/viewmodel/src/main/java/jatx/russianrocksongbook/viewmodel/CommonViewModelDeps.kt
+++ presentation-layer/viewmodel/src/main/java/jatx/russianrocksongbook/viewmodel/CommonViewModelDeps.kt
@@ -1,21 +1,20 @@
 package jatx.russianrocksongbook.viewmodel
 
-import android.content.Context
-import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.android.scopes.ViewModelScoped
 import jatx.russianrocksongbook.domain.repository.preferences.SettingsRepository
 import jatx.russianrocksongbook.domain.usecase.local.GetArtistsUseCase
 import jatx.russianrocksongbook.viewmodel.deps.Callbacks
 import jatx.russianrocksongbook.viewmodel.deps.Resources
+import jatx.russianrocksongbook.viewmodel.deps.TVDetector
 import jatx.russianrocksongbook.viewmodel.deps.Toasts
 import javax.inject.Inject
 
 @ViewModelScoped
 class CommonViewModelDeps @Inject constructor(
-    @ApplicationContext val appContext: Context,
     val settingsRepository: SettingsRepository,
     val callbacks: Callbacks,
     val resources: Resources,
     val toasts: Toasts,
+    val tvDetector: TVDetector,
     val getArtistsUseCase: GetArtistsUseCase
 )
---
+++ presentation-layer/viewmodel/src/main/java/jatx/russianrocksongbook/viewmodel/deps/TVDetector.kt
@@ -0,0 +1,5 @@
+package jatx.russianrocksongbook.viewmodel.deps
+
+interface TVDetector {
+    val isTV: Boolean
+}
