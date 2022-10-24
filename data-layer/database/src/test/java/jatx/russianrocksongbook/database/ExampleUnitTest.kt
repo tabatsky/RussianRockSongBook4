abf5338aa8c029e09eb25a5dbab750a20b666b6c
Evgeniy Tabatsky <tabatsky.evgeniy@gene.com>
2022-10-24 10:36:30 +1000

    data layer database unit tests.

---
+++ data-layer/database/src/test/java/jatx/russianrocksongbook/database/ExampleUnitTest.kt
@@ -0,0 +1,87 @@
+package jatx.russianrocksongbook.database
+
+import jatx.russianrocksongbook.database.converters.toSong
+import jatx.russianrocksongbook.database.converters.toSongEntity
+import jatx.russianrocksongbook.database.converters.asSongWithArtist
+import jatx.russianrocksongbook.database.db.entities.SongEntity
+import jatx.russianrocksongbook.database.gson.SongGson
+import jatx.russianrocksongbook.domain.models.local.Song
+import jatx.russianrocksongbook.domain.models.local.songTextHash
+import org.junit.Assert.assertEquals
+import org.junit.Test
+
+const val ID = 137L
+const val ARTIST = "some artist"
+const val TITLE = "some title"
+const val TEXT = "some text"
+const val FAVORITE = true
+const val DELETED = false
+const val OUT_OF_THE_BOX = true
+val ORIG_TEXT_MD5 = songTextHash(TEXT)
+
+class ExampleUnitTest {
+    @Test
+    fun song_toSongEntity_isWorkingCorrect() {
+        val song = Song(
+            id = ID,
+            artist = ARTIST,
+            title = TITLE,
+            text = TEXT,
+            favorite = FAVORITE,
+            deleted = DELETED,
+            outOfTheBox = OUT_OF_THE_BOX,
+            origTextMD5 = ORIG_TEXT_MD5
+        )
+
+        val songEntity = song.toSongEntity()
+
+        assertEquals(songEntity.id, ID)
+        assertEquals(songEntity.artist, ARTIST)
+        assertEquals(songEntity.title, TITLE)
+        assertEquals(songEntity.text, TEXT)
+        assertEquals(songEntity.favorite, FAVORITE)
+        assertEquals(songEntity.deleted, DELETED)
+        assertEquals(songEntity.outOfTheBox, OUT_OF_THE_BOX)
+        assertEquals(songEntity.origTextMD5, ORIG_TEXT_MD5)
+    }
+
+    @Test
+    fun songEntity_toSong_isWorkingCorrect() {
+        val songEntity = SongEntity(
+            id = ID,
+            artist = ARTIST,
+            title = TITLE,
+            text = TEXT,
+            favorite = FAVORITE,
+            deleted = DELETED,
+            outOfTheBox = OUT_OF_THE_BOX,
+            origTextMD5 = ORIG_TEXT_MD5
+        )
+
+        val song = songEntity.toSong()
+
+        assertEquals(song.id, ID)
+        assertEquals(song.artist, ARTIST)
+        assertEquals(song.title, TITLE)
+        assertEquals(song.text, TEXT)
+        assertEquals(song.favorite, FAVORITE)
+        assertEquals(song.deleted, DELETED)
+        assertEquals(song.outOfTheBox, OUT_OF_THE_BOX)
+        assertEquals(song.origTextMD5, ORIG_TEXT_MD5)
+    }
+
+    @Test
+    fun songGson_asSongWithArtist_isWorkingCorrect() {
+        val songGson = SongGson(
+            title = TITLE,
+            text = TEXT
+        )
+
+        val song = songGson asSongWithArtist ARTIST
+
+        assertEquals(song.artist, ARTIST)
+        assertEquals(song.title, TITLE)
+        assertEquals(song.text, TEXT)
+        assertEquals(song.origTextMD5, ORIG_TEXT_MD5)
+    }
+}
