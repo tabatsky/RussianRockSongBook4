366fa9bcb00e3189a1671479f272aa19199ab28d
Evgeniy Tabatsky <tabatsky.evgeniy@gene.com>
2022-10-25 11:22:41 +1000

    a little changes.

--- data-layer/networking/src/main/java/jatx/russianrocksongbook/networking/converters/AppCrash.kt
+++ data-layer/networking/src/main/java/jatx/russianrocksongbook/networking/converters/AppCrash.kt
@@ -4,5 +4,10 @@ import jatx.russianrocksongbook.domain.models.appcrash.AppCrash
 import jatx.russianrocksongbook.networking.gson.AppCrashGson
 
 internal fun AppCrash.toAppCrashJson() = AppCrashGson(
-    appVersionName, appVersionCode, androidVersion, manufacturer, product, stackTrace
+    appVersionName = appVersionName,
+    appVersionCode = appVersionCode,
+    androidVersion = androidVersion,
+    manufacturer = manufacturer,
+    product = product,
+    stackTrace = stackTrace
 )
