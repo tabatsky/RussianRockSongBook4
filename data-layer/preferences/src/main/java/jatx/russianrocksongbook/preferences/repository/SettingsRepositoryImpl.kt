b1be2f203c9703896c5d197e43db005c2d815ebb
Evgeniy Tabatsky <tabatsky.evgeniy@gene.com>
2023-07-13 09:53:15 +1000

    SettingsStorage separated from SettingsRepositoryImpl

--- data-layer/preferences/src/main/java/jatx/russianrocksongbook/preferences/repository/SettingsRepositoryImpl.kt
+++ data-layer/preferences/src/main/java/jatx/russianrocksongbook/preferences/repository/SettingsRepositoryImpl.kt
@@ -1,171 +1,84 @@
 package jatx.russianrocksongbook.preferences.repository
 
-import android.annotation.SuppressLint
-import android.content.Context
-import android.os.Build
-import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
 import it.czerwinski.android.hilt.annotations.BoundTo
 import jatx.russianrocksongbook.domain.repository.preferences.*
+import jatx.russianrocksongbook.preferences.storage.SettingsStorage
 import javax.inject.Inject
 import javax.inject.Singleton
 import kotlin.math.pow
 
-internal const val PREFS_NAME = "RussianRockPreferences"
-
-internal const val KEY_APK_VERSION = "apkVersion"
-internal const val KEY_THEME = "theme_int"
-internal const val KEY_ORIENTATION = "orientation_int"
-internal const val KEY_DEFAULT_ARTIST = "defaultArtist"
-internal const val KEY_FONT_SCALE = "fontScale"
-internal const val KEY_LISTEN_TO_MUSIC_VARIANT = "listenToMusicVariant"
-internal const val KEY_SCROLL_SPEED = "scrollSpeed"
-internal const val KEY_YOUTUBE_MUSIC_DONT_ASK = "youtubeMusicDontAsk"
-internal const val KEY_VK_MUSIC_DONT_ASK = "vkMusicDontAsk"
-internal const val KEY_YANDEX_MUSIC_DONT_ASK = "yandexMusicDontAsk"
-internal const val KEY_VOICE_HELP_DONT_ASK = "voiceHelpDontAsk"
-
 @Singleton
 @BoundTo(supertype = SettingsRepository::class, component = SingletonComponent::class)
-@SuppressLint("ApplySharedPref")
 class SettingsRepositoryImpl @Inject constructor(
-    @ApplicationContext private val context: Context
+    private val settingsStorage: SettingsStorage
 ): SettingsRepository {
-    private val sp = context.getSharedPreferences(PREFS_NAME, 0)
 
-    @Suppress("DEPRECATION")
     override val appWasUpdated: Boolean
-        get() {
-            return try {
-                val pInfo =
-                    context.packageManager.getPackageInfo(context.packageName, 0)
-                val newVersion = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) {
-                    pInfo.longVersionCode.toInt()
-                } else {
-                    pInfo.versionCode
-                }
+        get() = settingsStorage.appWasUpdated
 
-                val oldVersion = sp.getInt(KEY_APK_VERSION, 0)
-
-                (newVersion > oldVersion)
-            } catch (e: Throwable) {
-                false
-            }
-        }
-
-
-    @Suppress("DEPRECATION")
-    override fun confirmAppUpdate() {
-        try {
-            val pInfo =
-                context.packageManager.getPackageInfo(context.packageName, 0)
-            val newVersion = pInfo.versionCode
-
-            val editor = sp.edit()
-            editor.putInt(KEY_APK_VERSION, newVersion)
-            editor.commit()
-        } catch (_: Throwable) {}
-    }
+    override fun confirmAppUpdate() = settingsStorage.confirmAppUpdate()
 
     override var theme: Theme
-        get() = Theme.values()[_theme]
+        get() = Theme.values()[settingsStorage.theme]
         set(value) {
-            _theme = value.ordinal
-        }
-
-    private var _theme: Int
-        get() = sp.getInt(KEY_THEME, 0)
-        set(value) {
-            val editor = sp.edit()
-            editor.putInt(KEY_THEME, value)
-            editor.commit()
+            settingsStorage.theme = value.ordinal
         }
 
     override var orientation: Orientation
-        get() = Orientation.values()[_orientation]
+        get() = Orientation.values()[settingsStorage.orientation]
         set(value) {
-            _orientation = value.ordinal
+            settingsStorage.orientation = value.ordinal
         }
-
-    private var _orientation: Int
-        get() = sp.getInt(KEY_ORIENTATION, 0)
-        set(value) {
-            val editor = sp.edit()
-            editor.putInt(KEY_ORIENTATION, value)
-            editor.commit()
-        }
-
 
     override var listenToMusicVariant: ListenToMusicVariant
         get() = ListenToMusicVariant
-            .values()[_listenToMusicVariant]
+            .values()[settingsStorage.listenToMusicVariant]
         set(value) {
-            _listenToMusicVariant = value.ordinal
-        }
-
-    private var _listenToMusicVariant: Int
-        get() = sp.getInt(KEY_LISTEN_TO_MUSIC_VARIANT, 1)
-        set(value) {
-            val editor = sp.edit()
-            editor.putInt(KEY_LISTEN_TO_MUSIC_VARIANT, value)
-            editor.commit()
+            settingsStorage.listenToMusicVariant = value.ordinal
         }
 
     override var defaultArtist: String
-        get() = sp.getString(KEY_DEFAULT_ARTIST, null) ?: ARTIST_KINO
+        get() = settingsStorage.defaultArtist
         set(value) {
-            val editor = sp.edit()
-            editor.putString(KEY_DEFAULT_ARTIST, value)
-            editor.commit()
+            settingsStorage.defaultArtist = value
         }
 
     override var scrollSpeed: Float
-        get() = sp.getFloat(KEY_SCROLL_SPEED, 1.0f)
+        get() = settingsStorage.scrollSpeed
         set(value) {
-            val editor = sp.edit()
-            editor.putFloat(KEY_SCROLL_SPEED, value)
-            editor.commit()
+            settingsStorage.scrollSpeed = value
         }
 
     override var youtubeMusicDontAsk: Boolean
-        get() = sp.getBoolean(KEY_YOUTUBE_MUSIC_DONT_ASK, false)
+        get() = settingsStorage.youtubeMusicDontAsk
         set(value) {
-            val editor = sp.edit()
-            editor.putBoolean(KEY_YOUTUBE_MUSIC_DONT_ASK, value)
-            editor.commit()
+            settingsStorage.youtubeMusicDontAsk = value
         }
 
     override var vkMusicDontAsk: Boolean
-        get() = sp.getBoolean(KEY_VK_MUSIC_DONT_ASK, false)
+        get() = settingsStorage.vkMusicDontAsk
         set(value) {
-            val editor = sp.edit()
-            editor.putBoolean(KEY_VK_MUSIC_DONT_ASK, value)
-            editor.commit()
+            settingsStorage.vkMusicDontAsk = value
         }
 
     override var yandexMusicDontAsk: Boolean
-        get() = sp.getBoolean(KEY_YANDEX_MUSIC_DONT_ASK, false)
+        get() = settingsStorage.yandexMusicDontAsk
         set(value) {
-            val editor = sp.edit()
-            editor.putBoolean(KEY_YANDEX_MUSIC_DONT_ASK, value)
-            editor.commit()
+            settingsStorage.yandexMusicDontAsk = value
         }
 
     override var voiceHelpDontAsk: Boolean
-        get() = sp.getBoolean(KEY_VOICE_HELP_DONT_ASK, false)
+        get() = settingsStorage.voiceHelpDontAsk
         set(value) {
-            val editor = sp.edit()
-            editor.putBoolean(KEY_VOICE_HELP_DONT_ASK, value)
-            editor.commit()
+            settingsStorage.voiceHelpDontAsk = value
         }
 
 
     override var commonFontScale: Float
-        get() = sp.getFloat(KEY_FONT_SCALE, 1.0f)
+        get() = settingsStorage.commonFontScale
         set(value) {
-            val editor = sp.edit()
-            editor.putFloat(KEY_FONT_SCALE, value)
-            editor.commit()
+            settingsStorage.commonFontScale = value
         }
 
     override val commonFontScaleEnum: FontScale
