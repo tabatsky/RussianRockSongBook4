d3394623ab2562c941c7e89d8e1d3d821d66d2de
Evgeniy Tabatsky <tabatsky.evgeniy@gene.com>
2023-08-31 09:56:44 +1000

    some little changes

--- presentation-layer/addSong/src/main/java/jatx/russianrocksongbook/addsong/internal/viewmodel/AddSongUIState.kt
+++ presentation-layer/addSong/src/main/java/jatx/russianrocksongbook/addsong/internal/viewmodel/AddSongUIState.kt
@@ -1,11 +1,12 @@
 package jatx.russianrocksongbook.addsong.internal.viewmodel
 
+import jatx.russianrocksongbook.commonviewmodel.UIState
 import jatx.russianrocksongbook.domain.models.local.Song
 
 data class AddSongUIState(
     val showUploadDialogForSong: Boolean,
     val newSong: Song?
-) {
+): UIState {
     companion object {
         fun initial() = AddSongUIState(
             showUploadDialogForSong = false,
--- presentation-layer/start/src/main/java/jatx/russianrocksongbook/start/internal/viewmodel/StartUIState.kt
+++ presentation-layer/start/src/main/java/jatx/russianrocksongbook/start/internal/viewmodel/StartUIState.kt
@@ -1,9 +1,11 @@
 package jatx.russianrocksongbook.start.internal.viewmodel
 
+import jatx.russianrocksongbook.commonviewmodel.UIState
+
 data class StartUIState(
     val stubCurrentProgress: Int,
     val stubTotalProgress: Int
-) {
+): UIState {
     companion object {
         fun initial() = StartUIState(
             stubCurrentProgress = 0,
