5a158cb4bd389ba0ef0fda053ce77765af315f8d
Evgeniy Tabatsky <tabatsky.evgeniy@gene.com>
2024-02-10 09:12:49 +1000

    new artist added

--- data-layer/database/src/main/java/jatx/russianrocksongbook/database/dbinit/JsonResourceLoader.kt
+++ data-layer/database/src/main/java/jatx/russianrocksongbook/database/dbinit/JsonResourceLoader.kt
@@ -73,6 +73,7 @@ internal val artistMap = mapOf(
     "ДДТ" to R.raw.ddt,
     "Дельфин" to R.raw.dolphin,
     "Дом Кукол" to R.raw.dom_kukol,
+    "Екатерина Яшникова" to R.raw.yashnikova,
     "Звери" to R.raw.zveri,
     "Звуки Му" to R.raw.zvukimu,
     "Земляне" to R.raw.zemlane,
