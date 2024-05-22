572d3dbb37eb6ea7183576c90eef6bc1d44fb880
Evgeniy Tabatsky <tabatsky.evgeniy@gene.com>
2024-05-23 05:57:01 +1000

    delete from cloud: coroutine instead of RxJava

--- domain/usecase/cloud/src/main/java/jatx/russianrocksongbook/domain/usecase/cloud/DeleteFromCloudUseCase.kt
+++ domain/usecase/cloud/src/main/java/jatx/russianrocksongbook/domain/usecase/cloud/DeleteFromCloudUseCase.kt
@@ -9,6 +9,6 @@ import javax.inject.Singleton
 class DeleteFromCloudUseCase @Inject constructor(
     private val cloudRepository: CloudRepository
 ) {
-    fun execute(secret1: String, secret2: String, cloudSong: CloudSong) = cloudRepository
+    suspend fun execute(secret1: String, secret2: String, cloudSong: CloudSong) = cloudRepository
         .delete(secret1, secret2, cloudSong)
 }
