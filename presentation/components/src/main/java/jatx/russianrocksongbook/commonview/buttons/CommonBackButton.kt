9970c110cff85bf93f1773bd0b830f1289e30b0f
Evgeniy Tabatsky <tabatsky.evgeniy@gene.com>
2024-11-12 07:15:11 +1000

    common song text screen impl preview added

--- presentation/components/src/main/java/jatx/russianrocksongbook/commonview/buttons/CommonBackButton.kt
+++ presentation/components/src/main/java/jatx/russianrocksongbook/commonview/buttons/CommonBackButton.kt
@@ -7,10 +7,10 @@ import jatx.russianrocksongbook.testing.BACK_BUTTON
 import jatx.russianrocksongbook.commonviewmodel.CommonViewModel
 
 @Composable
-fun CommonBackButton(
-    commonViewModel: CommonViewModel = CommonViewModel.getInstance()
-) {
-    val onBackClick = { commonViewModel.submitAction(Back()) }
+fun CommonBackButton() {
+    val onBackClick = {
+        CommonViewModel.getStoredInstance()?.submitAction(Back()) ?: Unit
+    }
     CommonIconButton(
         resId = R.drawable.ic_back,
         testTag = BACK_BUTTON,
