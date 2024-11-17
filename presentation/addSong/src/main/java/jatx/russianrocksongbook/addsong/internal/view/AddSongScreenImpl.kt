ce56690b6708b67819dd2e82caa94ed00b3bbbfa
Evgeniy Tabatsky <tabatsky.evgeniy@gene.com>
2024-11-18 08:17:56 +1000

    preview for add song screen impl

--- presentation/addSong/src/main/java/jatx/russianrocksongbook/addsong/internal/view/AddSongScreenImpl.kt
+++ presentation/addSong/src/main/java/jatx/russianrocksongbook/addsong/internal/view/AddSongScreenImpl.kt
@@ -1,86 +1,27 @@
 package jatx.russianrocksongbook.addsong.internal.view
 
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.BoxWithConstraints
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.*
-import androidx.compose.runtime.saveable.rememberSaveable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.stringResource
-import jatx.russianrocksongbook.addsong.R
 import jatx.russianrocksongbook.addsong.internal.viewmodel.AddSongViewModel
-import jatx.russianrocksongbook.addsong.internal.viewmodel.HideUploadOfferForSong
-import jatx.russianrocksongbook.addsong.internal.viewmodel.Reset
-import jatx.russianrocksongbook.addsong.internal.viewmodel.ShowNewSong
-import jatx.russianrocksongbook.addsong.internal.viewmodel.UploadNewToCloud
-import jatx.russianrocksongbook.commonview.appbar.CommonSideAppBar
-import jatx.russianrocksongbook.commonview.appbar.CommonTopAppBar
-import jatx.russianrocksongbook.commonview.dialogs.confirm.UploadDialog
 
 @Composable
 internal fun AddSongScreenImpl() {
     val addSongViewModel = AddSongViewModel.getInstance()
 
-    var screenInitDone by rememberSaveable { mutableStateOf(false) }
+    val artistState = addSongViewModel.artist
+    val titleState = addSongViewModel.title
+    val textState = addSongViewModel.text
+    val addSongStateState = addSongViewModel.addSongStateFlow.collectAsState()
+    val submitAction = addSongViewModel::submitAction
+    val submitEffect = addSongViewModel::submitEffect
 
-    if (!screenInitDone) {
-        LaunchedEffect(Unit) {
-            screenInitDone = true
-            addSongViewModel.submitAction(Reset)
-        }
-    }
-
-    val addSongState by addSongViewModel.addSongStateFlow.collectAsState()
-    val showUploadDialog = addSongState.showUploadDialogForSong
-
-    val theme = addSongViewModel.theme.collectAsState().value
-
-    BoxWithConstraints(
-        modifier = Modifier
-            .fillMaxSize()
-    ) {
-        val W = this.maxWidth
-        val H = this.maxHeight
-
-        if (W < H) {
-            Column(
-                modifier = Modifier
-                    .fillMaxSize()
-                    .background(color = theme.colorBg)
-            ) {
-                CommonTopAppBar(title = stringResource(id = R.string.title_add_song))
-                AddSongBody()
-            }
-        } else {
-            Row(
-                modifier = Modifier
-                    .fillMaxSize()
-                    .background(color = theme.colorBg)
-            ) {
-                CommonSideAppBar(title = stringResource(id = R.string.title_add_song))
-                AddSongBody()
-            }
-        }
-
-        if (showUploadDialog) {
-            UploadDialog(
-                onConfirm = {
-                    addSongViewModel.submitAction(UploadNewToCloud)
-                },
-                onDecline = {
-                    addSongViewModel.submitAction(ShowNewSong)
-                },
-                onDismiss = {
-                    addSongViewModel.submitAction(HideUploadOfferForSong)
-                }
-            )
-        }
-    }
+    AddSongScreenImplContent(
+        artistState = artistState,
+        titleState = titleState,
+        textState = textState,
+        addSongStateState = addSongStateState,
+        submitAction = submitAction,
+        submitEffect = submitEffect
+    )
 }
 
