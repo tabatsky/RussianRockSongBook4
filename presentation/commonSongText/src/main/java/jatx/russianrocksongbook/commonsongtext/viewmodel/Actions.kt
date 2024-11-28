61c39cc82ab3ddb1b433ece407d46e5a4aa138e1
Evgeniy Tabatsky <tabatsky.evgeniy@gene.com>
2024-11-28 16:45:00 +1000

    some actions are moved between modules

---
+++ presentation/commonSongText/src/main/java/jatx/russianrocksongbook/commonsongtext/viewmodel/Actions.kt
@@ -0,0 +1,14 @@
+package jatx.russianrocksongbook.commonsongtext.viewmodel
+
+import jatx.russianrocksongbook.commonviewmodel.UIAction
+import jatx.russianrocksongbook.domain.models.local.Song
+
+data class UpdateCurrentSong(val song: Song?): UIAction
+data class SelectSong(val position: Int): UIAction
+object NextSong: UIAction
+object PrevSong: UIAction
+data class SetEditorMode(val isEditor: Boolean): UIAction
+data class SetAutoPlayMode(val isAutoPlay: Boolean): UIAction
+data class SaveSong(val song: Song): UIAction
+data class SetFavorite(val favorite: Boolean): UIAction
+object UploadCurrentToCloud: UIAction
